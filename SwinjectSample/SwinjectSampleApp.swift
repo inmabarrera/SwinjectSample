efda82da44149a14b3b12207f4f220aba0439d14
Inma Barrera <inma.barrera.ramirez@gmail.com>
2024-10-16 00:26:56 +0200

    Project init

---
+++ SwinjectSample/SwinjectSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwinjectSampleApp.swift
+//  SwinjectSample
+//
+//  Created by Inma on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwinjectSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
